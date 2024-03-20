a07cbdf956f06388bdcfde4332820e0011663be0
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-03-20 19:25:55 -0400

    Created Truck class

---
+++ Vechile Rental Management/Truck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Truck : Vehicle
+{
+    private double cargoCapacity;
+
+    public double CargoCapacity
+    {
+        get { return cargoCapacity; }
+        set { cargoCapacity = value; }
+    }
+
+    public Truck(string model, string manufacturer, int year, double rentalPrice, double cargoCapacity)
+        : base(model, manufacturer, year, rentalPrice)
+    {
+        CargoCapacity = cargoCapacity;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Cargo Capacity: {CargoCapacity}");
+    }
+}
