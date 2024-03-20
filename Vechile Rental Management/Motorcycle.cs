6b5f6df12715c19b79e86bba16c87843f57be308
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-03-20 19:27:05 -0400

    Created Motorcycle class

---
+++ Vechile Rental Management/Motorcycle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Motorcycle : Vehicle
+{
+    private string type;
+
+    public string Type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+
+    public Motorcycle(string model, string manufacturer, int year, double rentalPrice, string type)
+        : base(model, manufacturer, year, rentalPrice)
+    {
+        Type = type;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Type: {Type}");
+    }
+}
