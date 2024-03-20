c4b5355b828d03f348ae3f5266b371099e8c56b9
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-03-20 19:24:05 -0400

    Created Vechile and Car  class

---
+++ Vechile Rental Management/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Car : Vehicle
+{
+    private int numberOfSeats;
+
+    public int NumberOfSeats
+    {
+        get { return numberOfSeats; }
+        set { numberOfSeats = value; }
+    }
+
+    public Car(string model, string manufacturer, int year, double rentalPrice, int numberOfSeats)
+        : base(model, manufacturer, year, rentalPrice)
+    {
+        NumberOfSeats = numberOfSeats;
+    }
+
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine($"Number of Seats: {NumberOfSeats}");
+    }
+}
---
+++ Vechile Rental Management/Vechile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Vehicle
+{
+    private string model;
+    private string manufacturer;
+    private int year;
+    private double rentalPrice;
+
+    public string Model
+    {
+        get { return model; }
+        set { model = value; }
+    }
+
+    public string Manufacturer
+    {
+        get { return manufacturer; }
+        set { manufacturer = value; }
+    }
+
+    public int Year
+    {
+        get { return year; }
+        set { year = value; }
+    }
+
+    public double RentalPrice
+    {
+        get { return rentalPrice; }
+        set { rentalPrice = value; }
+    }
+
+    public Vehicle(string model, string manufacturer, int year, double rentalPrice)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalPrice;
+    }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice}");
+    }
+}
