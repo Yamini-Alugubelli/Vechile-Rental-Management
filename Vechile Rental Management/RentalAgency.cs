292a08038add607b549a9e701f51a7a4e0c174a2
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-03-20 19:30:02 -0400

    Created RentalAgency class

---
+++ Vechile Rental Management/RentalAgency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class RentalAgency
+{
+    private List<Vehicle> vehicles;
+
+    public RentalAgency()
+    {
+        vehicles = new List<Vehicle>();
+    }
+
+    public void AddVehicle(Vehicle vehicle)
+    {
+        vehicles.Add(vehicle);
+    }
+
+    public void DisplayAllVehiclesDetails()
+    {
+        foreach (var vehicle in vehicles)
+        {
+            vehicle.DisplayDetails();
+        }
+    }
+}
